245ab22f1225a42eb695a2404ee89f1694889698
Moonwalker231102 <108191264+Moonwalker231102@users.noreply.github.com>
2023-01-11 04:18:09 -0500

    Hoework 04-JS-III finished

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -154,9 +154,16 @@ function diaDeLaSemana(numeroDeDia) {
 
 
 function empiezaConNueve(n) {
-  //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
+  //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  
+  const numero = n.toString();
+  if (numero[0] === '9') {
+    return true;
+  } else { 
+    return false;
+  }
   
 }
 
@@ -165,15 +172,32 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+
+  const firstOfArreglo = arreglo[0];
   
+  for (let i = 0; i < arreglo.length; i++) {
+    if (firstOfArreglo !== arreglo[i]) {
+      return false;
+    }
+  } return true;
 } 
 
 
 function mesesDelAño(array) {
-  //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
+  //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  const mesesResultado = [];
+  for (let i = 0; i < array.length; i++) { 
+    if (array [i] === "Enero" || array [i] === "Marzo" || array [i] === "Noviembre") { 
+      mesesResultado.push(array[i]);
+    }
+    if (mesesResultado.length === 3) {
+      return mesesResultado;
+    }
+  } 
+  return "No se encontraron los meses pedidos";
 }
 
 
@@ -181,6 +205,13 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  const mayores = [];
+  for (let i = 0; i < array.length; i++) { 
+    if (array[i] > 100) { 
+      mayores.push(array[i]);
+    } 
+
+  }    return mayores; 
 }
 
 
@@ -192,6 +223,19 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  
+  const numerosResultado = [];
+  for (var i = 0; i < 10; i++) { 
+    numero += 2;
+    numerosResultado.push(numero);
+
+    if (numero === i + 1) { 
+      break;
+    }
+  }
+  if (i < 10) { 
+    return "Se interrumpió la ejecución";
+  } return numerosResultado;
 }
 
 
@@ -202,6 +246,15 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  const nuevosNumeros = [];
+  for (var i = 0; i < 10; i++) {
+    if (i + 1 === 5) { 
+      continue; 
+    }
+    numero += 2;
+    nuevosNumeros.push(numero);
+  }
+  return nuevosNumeros;
 }
 
 
